454da90dc28238fc1dfcd186e22c6467215a8d59
lodashreload <icarusjapan@gmail.com>
2021-12-01 22:41:34 -0500

    First attempt...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,117 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BlackJackCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    //   public enum CardSuit
+    // {
+    //     Hearts,
+    //     Clubs,
+    //     Diamonds,
+    //     Spades
+    // }
+
+    // public enum CardValue
+    // {
+    //     Ace = 1,
+    //     Two = 2,
+    //     Three = 3,
+    //     Four = 4,
+    //     Five = 5,
+    //     Six = 6,
+    //     Seven = 7,
+    //     Eight = 8,
+    //     Nine = 9,
+    //     Ten = 10,
+    //     Jack = 10,
+    //     Queen = 10,
+    //     King = 10
+    // }
+    class card
     {
-      Console.WriteLine("Welcome to C#");
+        public string Suit { get; set; }
+        public string Rank { get; set; }
+        public int CardValue { get; set; }
     }
-  }
+
+    // class hand
+    // {
+    //     public string card { get; set; } = card.new (suit, Rank, CardValue);
+    // }
+    public class CardDeck
+    {
+
+    }
+    class Program
+    {
+
+        //^^^^^^^^^^^^^^Classes^^^^^^^^^^^^^^^^^^^^    
+        static void Main(string[] args)
+        {
+
+            // Algorithm B 
+            // - Make a new list of the fours suits
+            var suits = new List<string>() { "clubs", "diamonds", "hearts", "spades" };
+            // - Make of a list of 13 ranks and call this list ranks
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            // - Make a new list of strings namer 'deck'
+            var deck = new List<string>();
+            // - Make a loop that goes through all the suits
+            foreach (var suit in suits)
+            {
+
+                //   Make a loop that goes through all the 'ranks'
+                //   add newly formed string to the end of the deck list - For Suit = Clubs
+                foreach (var rank in ranks)
+                {
+                    var card = $"{rank} of {suit}";
+
+                    deck.Add(card);
+                }
+            }
+            var numberOfCards = deck.Count;
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
+            {
+                //   leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer"
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                // leftIndex = random integer >= 0 and < rightIndex
+                //   Now swap the values at rightIndex and leftIndex by doing this:
+                //   leftCard = the value from deck[leftIndex]
+                var leftCard = deck[leftIndex];
+                //   rightCard = the value from deck[rightIndex]
+                var rightCard = deck[rightIndex];
+                //   deck[rightIndex] = leftCard
+                deck[rightIndex] = leftCard;
+                //   deck[leftIndex] = rightCard
+                deck[leftIndex] = rightCard;
+
+            }
+            foreach (var card in deck)
+            {
+                //  Console.WriteLine(card);
+            }
+
+
+            // - first card = deck[0]
+            var firstcardB = deck[0] + deck[3];
+            // - second card = deck[1]
+            var secondcardB = deck[1];
+            // - print first and second card  
+            Console.WriteLine("firstcard: " + firstcardB);
+            // Console.WriteLine("Secondcard: " + secondcardB);
+
+            //Creating 1 hand (lists) of five cards and displaying the hands
+            //var hand1 = deck[0][1];
+            //This cannot be the best way of doing this, but i was failing at making a new list that would be better
+            // populated with cards with a random index.  I don't have the time to spend on this now and 
+            // will come back to it at some point.  Also need to figure out how to format text the way i want
+            // Console.WriteLine("Player 1: " + deck[0] + deck[1] + deck[2] + deck[3] + deck[4]);
+            // Console.WriteLine("Player 2: " + deck[5] + deck[6] + deck[7] + deck[8] + deck[9]);
+
+            //var hand1 = deck[new List<string> = (2, 3, 4, 5)];
+
+            Console.WriteLine("You have reach a save point");
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # BlackJackCS
+
+FAIL LEARN GROW REPEAT
+
+DRY
+assignment
+Explorer Mode
+General Rules:
+
+    The game should be played with a standard deck of playing cards (52).
+    The house should be dealt two cards, hidden from the player until the house reveals its hand.
+    The player should be dealt two cards, visible to the player.
+    The player should have a chance to hit (i.e. be dealt another card) until they decide to stop or they bust (i.e. their total is over 21). At which point they lose regardless of the dealer's hand.
+    When the player stands, the house reveals its hand and hits (i.e. draw cards) until they have 17 or more.
+    If dealer goes over 21 the dealer loses.
+    The player should have two choices: "Hit" and "Stand."
+    Consider Aces to be worth 11, never one.
+    The app should display the winner. For this mode, the winner is who is closer to a blackjack (21) without going over.
+    Ties go to the DEALER.
+    There should be an option to play again. This should start a new game with a new full deck of 52 shuffled cards and new empty hands for the dealer and the player.
+
+Adventure Mode
+
+    Reveal one of the house's cards to the player when cards dealt.
+    Consider aces be worth one or 11.
+    Allow the player to "Split".
+    Improve the win requirements. From Wikipedia:
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+PEDAC
+Problem
+Create a shuffles deck of 52 cards
+deal 2 card to a player and dealer from deck
+Hide the dealers cards from player
+player should have the chance to add another card until they decide stop or go over 21
+Have dealer do the same sequence but must hit until >= 17 if dealer is over 21 then lose
+Display if player or dealer is closer to or = 21 with tie going to dealer
+then need to restart the game
+optional
+Reveal one dealer card and hide the other
+Add a betting system that carries between rounds
+
+EXAMPLE
+dealer hand card + ? = dealer name hand = Jack of Spades ?
+player hand card + card = Player hand = 2 of hearts and Jack of Hearts
+DATA
+loops classes and objects
+objects
+1Card
+a.suit
+b.rank
+c.value
+
+2Deck(not an object but a list populated with an object maybe) will need to be in a list/array to keep track of cards?
+will need to shuffle
+will need to populate and draw (remove cards)
+
+3 (person) players and dealers
+keep two cards
+calculate the score of two cards and if the are busted
+
+Algorithm
+// create a deck of 52 cards
+// Display greeting "Hi my name is {dealer name} Are you ready to play some Black Jack? Hit enter to start"
+// Deal player two face up cards and display two cards for dealer one face up and one as ?
+// on players turn player can choose to HIT or stand
+// If HIT player is dealt another card to the hand
+// sum of all card is added to determine if over 21
+// if not over 21 player will be given the choice to hit or stand
+// if over 21 displayed message You lose
+// if player stands Then dealers turn
+// on dealers turn Display both cards dealt earlier turn ? to a card
+// The dealer will HIT until card score is great than or equal to 17 and then stand
+// The value of the players hand and dealers hand will be compared to see which on is higher but less than 21
+// Whichever hand is higher but not over 21 will be displayed in **\_** in the winner
+// Question will be displayed would you like to play again
+//
+Code
